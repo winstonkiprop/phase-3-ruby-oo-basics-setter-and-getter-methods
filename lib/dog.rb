8553c76cb2a96a7f3e890368dfaf2d180718611f
winston kiprop <winstonekip50@gmail.com>
2022-08-22 22:39:53 +0300

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
 
+    def name=(name)
+        @name = name
+    end
+    def breed=(breed)
+        @breed = breed
+    end
+    def name
+        @name
+    end
+    def breed
+        @breed
+    end
 end
+rex=Dog.new
+rex.name="rex"
+rex.breed="pointer"
+puts rex.name
+puts rex.breed
